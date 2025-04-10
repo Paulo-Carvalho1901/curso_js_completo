bf263d615f5c903b6c0432a5c32587e91442f36f
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-09 22:30:37 -0300

    comentarios

---
+++ aula2/comentario.js
@@ -0,0 +1,19 @@
+// E escrendo comantario
+console.log('Hello, World'); // Aqui tem outro comentarios
+console.log('Linha 1')
+// console.log('Linha 2')
+
+/*
+console.log('Linha 3')
+// comentario aqui dentro
+console.log('Linha 4')
+*/
+console.log('Termina aqui')
+// vai ser ignorado
+
+/*
+Código de bloco 
+para anotações
+para explicar seu código 
+documentações
+*/
