823b617c529d71bf91030c6ec2d9c07d654b6d8d
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-04-09 22:00:28 -0300

    console.log

--- aula1/index.js
+++ aula1/index.js
@@ -1,1 +1,8 @@
-console.log();
+// Imprimindo saida de varios tipos de dados
+
+console.log('Paulo "Carvalho"'); // string
+console.log("Paulo 'Carvalho'"); // string
+console.log(`Paulo Carvalho`); // string
+console.log(123456, 15.45); // inteiro / float
+console.log(15.85, 1245, 155.45, 'Paulo'); // varios valores em um único console.log
+
